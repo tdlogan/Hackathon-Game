8a0a83dd7fcb7422954cae772d8c33ac2cb27987
Thomas Logan <tdlogan@Thomass-MacBook-Pro.local>
2015-03-26 20:59:37 -0700

    Added functionality to audio files

--- soundfx.js
+++ soundfx.js
@@ -1,10 +1,27 @@
 
-//Loading up sound files
-var sounds = [
-  {id:"cannon", src:"Sounds/cannon.mp3"}
-  {id:"hit", src:"Sounds/hit.mp3"}
-  {id:"aim", src:"Sounds/aim.mp3"}
-];
+
+function init() {
+
+    //Adding sounds to the file.
+    var audioPath = 'Sounds/';
+    var sounds = [
+      {id:"cannon", src:"cannon.mp3"},
+      {id:"hit", src:"hit.mp3"},
+      {id:"aim", src:"aim.mp3"}
+    ];
+    
+    //Loading the sounds.
+    createjs.Sound.addEventListener('fileload', handlLoad);
+    createjs.Sound.registerSounds(sounds, audioPath);
+
+};
+
+function handleLoad(event) {
+    createjs.Sound.play(/*sound*/);
+}
+
+
+
 
 
 <script src="http://code.createjs.com/soundjs-0.5.2.min.js"></script>
