cdee77e26039e2304cec6eac7efe7e07e9d78d12
Thomas Logan <tdlogan@Thomass-MacBook-Pro.local>
2015-03-26 20:47:35 -0700

    Adding my sound files to the project folder

---
+++ soundfx.js
@@ -0,0 +1,12 @@
+
+//Loading up sound files
+var sounds = [
+  {id:"cannon", src:"Sounds/cannon.mp3"}
+  {id:"hit", src:"Sounds/hit.mp3"}
+  {id:"aim", src:"Sounds/aim.mp3"}
+];
+
+
+<script src="http://code.createjs.com/soundjs-0.5.2.min.js"></script>
+
+
